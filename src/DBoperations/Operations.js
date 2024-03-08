95da1200dadf3c981cfd9b80d89a62575a814b80
Addisu544 <addisuagerie544@gmail.com>
2024-03-08 09:13:56 -0800

    client version 10

---
+++ src/DBoperations/Operations.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import InsertData from "./InsertData";
+import FetchData from "./FetchData";
+
+const Insert = () => {
+  return (
+    <>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/insert">Insert Data</Link>
+              </li>
+              <li>
+                <Link to="/fetch">Fetch Data</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/insert" element={<InsertData />} />
+            <Route path="/fetch" element={<FetchData />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+};
+
+export default Insert;
